2348fb991c85f19ca2c7018422fd25ab3141d142
ckwahome <ckwahome@gmail.com>
2025-11-02 09:21:00 +0000

    created hero section

---
+++ app/src/components/common/btn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Btn = () => {
+  return (
+    <div class="flex justify-center">
+        <button class="mt-6 px-5 py-1 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition ">
+            shop now
+        </button>
+    </div>
+  )
+}
--- app/src/components/sections/hero.jsx
+++ app/src/components/sections/hero.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Btn } from '../common/btn'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+ <>
+ <div class='bg-[#174143] bg-center bg-no-repeat h-full w-full bg-cover flex flex-col justify-center p-10 space-y-6'>
+ <h1 class='text-center text-[#F5E5E1] text-3xl'>Reduce,Reuse,Revamp!<br/>
+ Explore  our online thrift shop<br/>
+ For unique,eco-friendly fashion finds.<br/>
+ Shop with us and join the sustainable fashion movement  
+ </h1>
+
+<Btn/>
+
+ </div>
+ </>
   )
 }
